358fdd62d46328967a6ccc12b9b7e711009c0654
patw <patw.hi@gmail.com>
2016-06-23 20:21:06 +0800

    Main app

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,78 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import axios from 'axios';
+import querystring from 'querystring';
+import config from '../config';
+import { loginUser, logoutUser } from '../actions';
+import '../style/style.scss';
 
-export default class App extends Component {
+class App extends Component {
+  handleLogin() {
+    let code;
+    console.log(config);
+    window.location.href = `https://github.com/login/oauth/authorize?
+client_id=${config.client_id}&scopes=user&redirect_uri=${config.redirect_uri}`;
+  }
+
+  handleLogout() {
+    this.props.logoutUser();
+  }
+
+  componentWillMount() {
+    const query = window.location.search.substring(1);
+    const code = querystring.parse(query).code;
+    if (code) {
+      this.props.loginUser(code);
+    }
+  }
+
   render() {
+    let userTab = (
+      <a onClick={this.handleLogin.bind(this)} className="nav-link">Login</a>
+    );
+
+    if (this.props.token) {
+      userTab = (
+        <a onClick={this.handleLogout.bind(this)} className="nav-link">Logout</a>
+      );
+    }
+
     return (
       <div>
-        <h1>GitHub starred project viewer</h1>
+        <nav className="navbar navbar-fixed-top navbar-dark bg-inverse">
+          <Link className="navbar-brand" to="/">
+            GitHub Starred Project Viewer
+          </Link>
 
-        {this.props.children}
+          <ul className="nav navbar-nav">
+            <li className="nav-item">
+              {userTab}
+            </li>
+            <li className="nav-item">
+              <Link to="about" className="nav-link">About</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <main class="container">
+          {this.props.children}
+        </main>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    token: state.token,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+  {
+    loginUser,
+    logoutUser,
+  }
+)(App);
--- src/index.js
+++ src/index.js
@@ -5,18 +5,25 @@ import { createStore, applyMiddleware } from 'redux';
 import { Router, hashHistory, browserHistory } from 'react-router';
 import reduxThunk from 'redux-thunk';
 import routes from './routes';
-
+import { SAVE_ACCESS_TOKEN } from './actions/types';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 // Use hash location for Github Pages
 // but switch to HTML5 history locally.
 const history = process.env.NODE_ENV === 'production' ?
   hashHistory : browserHistory;
 
+const token = window.localStorage.getItem('token');
+// if token, we need update application state
+if (token) {
+  store.dispatch({ type: SAVE_ACCESS_TOKEN, payload: token });
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={history} routes={routes} />
   </Provider>
   , document.querySelector('.app'));
