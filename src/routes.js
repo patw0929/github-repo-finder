f02fd9fe36fe9a4edd098536d3127f82a0d1e41f
patw <patw.hi@gmail.com>
2016-06-23 20:32:16 +0800

    Move about component into components/

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/app';
-import About from './pages/about';
+import About from './components/about';
 
 export default (
   <Route path="/" component={App}>
