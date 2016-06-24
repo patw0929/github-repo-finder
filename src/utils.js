08f96b6485cbeb9ee9a4450f0b12b390604837bb
patw <patw.hi@gmail.com>
2016-06-24 19:57:11 +0800

    Fixed truncate function

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,16 @@
 export function parsePage(link) {
   try {
-    var pageInfo = link.split(',');
-    var pageLinks = [];
+    const pageInfo = link.split(',') || [];
+    const pageLinks = [];
+    const pages = {};
 
-    for (var i = 0, max = pageInfo.length; i < max; i++) {
+    for (let i = 0, max = pageInfo.length; i < max; i++) {
       pageLinks.push(pageInfo[i].split('; '));
     }
 
-    var pages = {};
-    for (var i = 0, max = pageLinks.length; i < max; i++) {
-      var key = pageLinks[i][1].replace(/rel=/ig, '').replace(/"/g, '');
-      var page = pageLinks[i][0].trim().replace(/.*page=(\d+)>+/ig, '$1');
+    for (let i = 0, max = pageLinks.length; i < max; i++) {
+      const key = pageLinks[i][1].replace(/rel=/ig, '').replace(/"/g, '');
+      const page = pageLinks[i][0].trim().replace(/.*page=(\d+)>+/ig, '$1');
 
       pages[key] = page;
     }
@@ -22,8 +22,12 @@ export function parsePage(link) {
 };
 
 export function truncate(n, useWordBoundary) {
+  if (!this) {
+    return null;
+  }
+
   const isTooLong = this.length > n;
-  let s_ = isTooLong ? this.substr(0,n-1) : this;
+  let s_ = isTooLong ? this.substr(0, n-1) : this;
   s_ = (useWordBoundary && isTooLong) ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
 
   return isTooLong ? s_ + '...' : s_;
