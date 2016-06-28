5ad0d55a0dfbfebf1ed34e5fb9227f9ccbf59876
patw <patw.hi@gmail.com>
2016-06-28 15:43:30 +0800

    Refine the router format (without queryKey)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { Router, hashHistory, browserHistory } from 'react-router';
+import { Router, useRouterHistory } from 'react-router';
+import { createHistory, createHashHistory } from 'history';
 import reduxThunk from 'redux-thunk';
 import routes from './routes';
 import { AUTH_USER } from './actions/types';
@@ -11,8 +12,10 @@ import reducers from './reducers';
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
 
-const history = process.env.NODE_ENV === 'production' ?
-  hashHistory : browserHistory;
+const historyType = process.env.NODE_ENV === 'production' ?
+  createHashHistory : createHistory;
+
+const history = useRouterHistory(historyType)({ queryKey: false })
 
 const token = window.localStorage.getItem('token');
 // if token, we need update application state
