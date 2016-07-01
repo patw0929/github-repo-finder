670bfd9563ee0e224807bc059365e3840890b118
patw <patw.hi@gmail.com>
2016-07-01 12:15:48 +0800

    Added 'create function script' into sql script

--- server/github_repo_finder.sql
+++ server/github_repo_finder.sql
@@ -1,3 +1,4 @@
+/* CREATE TABLE & DATA */
 CREATE TABLE tags (
   ID SERIAL PRIMARY KEY,
   repo VARCHAR,
@@ -9,3 +10,49 @@ CREATE TABLE tags (
 
 INSERT INTO tags (repo, username, tag, isPublic)
   VALUES ('patw0929/react-intl-tel-input', 'patw0929', 'telephone', true);
+
+
+
+/* CREATE FUNCTION */
+CREATE OR REPLACE FUNCTION fn_GetReposByTag
+(
+ Search_Tag VARCHAR
+ ,Paging_PageSize INTEGER = NULL
+ ,Paging_PageNumber INTEGER = NULL
+)
+RETURNS TABLE
+(
+ outRepoID INTEGER
+ ,outRepoName VARCHAR
+) AS
+$BODY$
+
+DECLARE PageNumber BIGINT;
+
+BEGIN
+ /* ***************************************************************
+ Construct Custom paging parameter...
+ **************************************************************** */
+ IF (paging_pagesize IS NOT NULL AND paging_pagenumber IS NOT NULL) THEN
+  PageNumber := (Paging_PageSize * (Paging_PageNumber-1));
+ END IF;
+
+ /* ************************************************
+ Custome paging SQL Query construction.......
+ ************************************************ */
+ RETURN QUERY
+ SELECT
+  id
+  ,repo
+ FROM public.tags
+ WHERE tag = Search_Tag
+ ORDER BY created_at DESC
+ LIMIT Paging_PageSize
+ OFFSET PageNumber;
+
+ EXCEPTION WHEN OTHERS THEN
+ RAISE;
+END;
+
+$BODY$
+LANGUAGE 'plpgsql';
